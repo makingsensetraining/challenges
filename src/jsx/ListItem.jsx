05ff89289fc78686d0557052d26b50feb66a06b7
NoeliaFranco <noeliasfranco@gmail.com>
2017-08-23 16:42:06 -0300

    React quiz app challenge

---
+++ src/jsx/ListItem.jsx
@@ -0,0 +1,6 @@
+/*
+    Create the ListItem
+    The return will be a li item
+    Each li will have an onClick method
+    Add the props validations
+ */
---
+++ src/jsx/Quiz.jsx
@@ -0,0 +1,64 @@
+import React, { PropTypes } from 'react';
+import Questions from './Questions.jsx';
+import AnswerList from './AnswerList.jsx';
+import Score from './Score.jsx';
+import Progress from './Progress.jsx';
+import '../App.css';
+
+class Quiz extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = this.getInitialState();
+
+    }
+
+
+    getInitialState() {
+        return {
+            questionData: [
+                {question: "This is the capital of which country? Canberra", answers: ["Turkey","Australia","Cuba","Algeria"], correct: 1},
+                {question: "This is the capital of which country? Bangkok", answers: ["Argentina","Thailand","India","United Kingdom"], correct: 1}],
+            progress: 0,
+            score: 0
+        };
+    }
+
+    checkAnswer(index) {
+        /*
+         Check if the answer is correct
+         If is correct, update the state with the new score and the new value
+         If is incorrect, refresh the progress
+        */
+    }
+
+    resetQuiz() {
+        /*
+        * Reset the state
+        * */
+    }
+
+    render() {
+        var currentQuestion = this.state.questionData[this.state.progress];
+        if(this.state.questionData.length > this.state.progress) {
+            return (
+                <div className="quiz container">
+                    <Questions questionText={currentQuestion.question} />
+                    <AnswerList answers={currentQuestion.answers} answerCallback={this.checkAnswer} />
+                    <Score score={this.state.score} />
+                    <Progress progress={this.state.progress} total={this.state.questionData.length}/>
+                </div>
+            );
+        } else {
+            return (
+                <div className="quiz container">
+                    <p className="quiz question">Quiz Finished!</p>
+                    <Score score={this.state.score} />
+                    <button type="button" className="quiz reset-btn" onClick={this.resetQuiz}>Reset Quiz</button>
+                </div>
+            );
+        }
+    }
+}
+
+export default Quiz;
